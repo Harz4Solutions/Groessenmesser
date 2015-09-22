f8543806cf3da590675af709510b4e22cce173cb
pinzellop <mxybxy@hotmail.ch>
2015-09-22 11:08:20 +0200

    refactored camera management, now a separate activity is used

---
+++ app/src/main/java/com/harz4solutions/groessenmesser/CameraActivity.java
@@ -0,0 +1,72 @@
+package com.harz4solutions.groessenmesser;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+public class CameraActivity extends AppCompatActivity {
+
+    private Camera mCamera;
+    private CameraView mPreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** TODO if **/
+
+        if(checkCameraHardware(getApplicationContext())){ //Check if Device has a Camera
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraView(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.cameraFrameLayout);
+            preview.addView(mPreview);
+        }
+
+    }
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+            c.setDisplayOrientation(90);
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/harz4solutions/groessenmesser/MainActivity.java
+++ app/src/main/java/com/harz4solutions/groessenmesser/MainActivity.java
@@ -1,56 +1,23 @@
 package com.harz4solutions.groessenmesser;
 
-import android.hardware.Camera;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private int counter = 0;
-    private Button button;
-    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent camera = new Intent(this, CameraActivity.class);
+        startActivity(camera);
+    }
 
-        final ProgressBar progressBar = (ProgressBar)findViewById(R.id.progressBar);
-        progressBar.setMax(10);
-        textView = (TextView) findViewById(R.id.textView);
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counter++;
-                progressBar.setProgress(counter);
-                if(counter==10){
-                    textView.setText(R.string.text2);
-                }
-                if(counter==11){
-                    counter=0;
-                    progressBar.setProgress(0);
-                    textView.setText(R.string.text1);
-                }
-            }
-        });
-    }
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
