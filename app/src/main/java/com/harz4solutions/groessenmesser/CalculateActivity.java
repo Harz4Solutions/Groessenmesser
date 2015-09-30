1501f7b47ccc4f186dd45ccac63bf6eebbcd80f6
boehis <sboehi@me.com>
2015-09-30 13:40:10 +0200

    Removed CameraActivity Refactored calculation

--- app/src/main/java/com/harz4solutions/groessenmesser/CalculateActivity.java
+++ app/src/main/java/com/harz4solutions/groessenmesser/CalculateActivity.java
@@ -3,7 +3,10 @@ package com.harz4solutions.groessenmesser;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -43,31 +46,44 @@ public class CalculateActivity extends Activity {
         betaTextfield.setText("" + beta);
 
         aTextfield.requestFocus();
-        aTextfield.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+
+        //Todo: Not working properly
+        aTextfield.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus){
-                    try {
-                        System.out.println(aTextfield.getText().toString());
-                        a = Double.parseDouble(aTextfield.getText().toString());
-                        b = calculateHeight();
-                        bTextfield.setText(""+b);
-                    }catch (NumberFormatException e){
-                        //ToDo: Some error handling
-                    }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                try {
+                    System.out.println(aTextfield.getText().toString());
+                    a = Double.parseDouble(aTextfield.getText().toString());
+                    b = calculateHeight();
+                    bTextfield.setText(""+b);
+                }catch (NumberFormatException e){
+                    //ToDo: Some error handling
                 }
             }
         });
-
-
     }
     public double calculateHeight(){
-        double gamma;
-        double c = a / Math.sin(alpha);
-        /*double h1 = Math.sqrt(c) - Math.sqrt(a);
-        double h2 = Math.sqrt(a / Math.cos(beta-(90-alpha))) - Math.sqrt(a);*/
-        gamma = 180 - (alpha + beta);
-        return (c / Math.sin(gamma)) * Math.sin(beta);
+        double radiantAlpha = alpha*(Math.PI / 180);
+        double radiantBeta = beta*(Math.PI / 180);
+        double radiantGamma = (180-alpha-beta)*(Math.PI / 180);
+        double c = a / Math.sin(radiantAlpha);
+        System.out.println("c:" + c);
+        double h1 = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
+        double h2 = Math.sqrt(Math.pow(c / Math.cos((beta-(90-alpha))*(Math.PI / 180)),2) - Math.pow(c,2));
+
+        System.out.println("b1:" + (h1 + h2));
+        System.out.println("b2:" + (c / Math.sin(radiantGamma)) * Math.sin(radiantBeta));
+        return (c / Math.sin(radiantGamma)) * Math.sin(radiantBeta);
     }
     public void noValuesFoundError(){
 
@@ -78,6 +94,20 @@ public class CalculateActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/harz4solutions/groessenmesser/MainActivity.java
+++ app/src/main/java/com/harz4solutions/groessenmesser/MainActivity.java
@@ -1,24 +1,149 @@
 package com.harz4solutions.groessenmesser;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private Camera mCamera;
+    private CameraView mPreview;
+    private TextView alphaText;
+    private TextView betaText;
+
+    private Button saveTopB;
+    private Button saveBottomB;
+
+    private double alpha;
+    private double alphaBeta;
+    private double beta;
+
+    private SensorManager sensorManager;
+    private Sensor orientationSensor;
+    private double sensorAngle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent camera = new Intent(this, CameraActivity.class);
-        startActivity(camera);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> orientationSensors= sensorManager.getSensorList(Sensor.TYPE_ORIENTATION);
+        orientationSensor = orientationSensors.get(0);
+
+        if(checkCameraHardware(getApplicationContext())){ //Check if Device has a Camera
+            // Create an instance of Camera
+            mCamera = getCameraInstance();
+
+            // Create our Preview view and set it as the content of our activity.
+            mPreview = new CameraView(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.cameraFrameLayout);
+            preview.addView(mPreview, 0);
+            View line = (View) findViewById(R.id.line);
+
+            alphaText = (TextView) findViewById(R.id.alpha);
+            alphaText.setText("\u03B1 : _");
+            betaText = (TextView) findViewById(R.id.beta);
+            betaText.setText("\u03B2 : _");
+
+
+            saveTopB = (Button) findViewById(R.id.saveTopLine);
+            saveTopB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alphaBeta = (sensorAngle>0) ? sensorAngle : sensorAngle*-1;;
+                    if (alpha != 0) {
+                        beta = alphaBeta - alpha;
+                        alphaText.setText("\u03B1 : " + (int) alpha);
+                        betaText.setText("\u03B2 : "+(int)beta);
+                        Button button = (Button) findViewById(R.id.calculate);
+                        button.setEnabled(true);
+                    }
+                }
+            });
+            saveBottomB = (Button) findViewById(R.id.saveBottomLine);
+            saveBottomB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    alpha = (sensorAngle > 0) ? sensorAngle : sensorAngle * -1;
+                    if (alphaBeta != 0) {
+                        beta = alphaBeta - alpha;
+                        alphaText.setText("\u03B1 : " + (int) alpha);
+                        betaText.setText("\u03B2 : "+(int)beta);
+                        Button button = (Button) findViewById(R.id.calculate);
+                        button.setEnabled(true);
+                    }
+                }
+            });
+
+            Button calculate = (Button) findViewById(R.id.calculate);
+            final Context context = this;
+            calculate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context,CalculateActivity.class);
+                    intent.putExtra("alpha",alpha);
+                    intent.putExtra("beta", beta);
+                    startActivity(intent);
+                }
+            });
+
+
+        }
+
+    }
+    public Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+            c.setDisplayOrientation(90);
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        sensorAngle = event.values[1];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, orientationSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,4 +166,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
